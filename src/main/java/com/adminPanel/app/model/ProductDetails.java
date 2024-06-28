a77cfe156745bc6122708c183014f773c402c5e8
Your Name <you@example.com>
2024-06-28 18:35:01 +0300

    added

---
+++ src/main/java/com/adminPanel/app/model/ProductDetails.java
@@ -0,0 +1,98 @@
+package com.adminPanel.app.model;
+
+
+import com.adminPanel.app.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.text.ParseException;
+import java.util.Date;
+
+@Entity
+@Table(name = "product_details")
+@NoArgsConstructor
+public class ProductDetails
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "expiration_date")
+    private Date expirationDate;
+
+    @Column(name = "manufacturer")
+    private String manufacturer;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "available")
+    private Boolean available;
+
+    @OneToOne(mappedBy = "productDetails" , cascade = CascadeType.ALL)
+    private Product product;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
